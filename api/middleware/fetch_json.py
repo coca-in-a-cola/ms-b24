5edca1438cdd74022041353774e5001de70a51b0
coca-in-a-cola <coca-in-a-cola@protonmail.com>
2022-08-22 12:34:50 +0800

    fix for sure

--- api/middleware/fetch_json.py
+++ api/middleware/fetch_json.py
@@ -8,7 +8,7 @@ def fetch_json(f):
     @functools.wraps(f)
     def decorated(*args, **kwargs):
         content_type = request.headers.get('Content-Type')
-        if (content_type != 'application/json' or content_type != 'application/json; charset=UTF-8'):
+        if (content_type != 'application/json' and content_type != 'application/json; charset=UTF-8'):
             return jsonify({
                     'error' : 'Ошибка! Отправляйте данные только через json'
             }), 400
